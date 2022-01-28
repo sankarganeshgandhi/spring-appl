2666e0e9ae00f396a959b015ff67738e7dd6aa4e
Sankarganesh Gandhi <sankarganesh.gandhi@gmail.com>
2022-01-28 11:35:12 +0100

    initial commit

---
+++ src/main/java/com/example/demo/LoggingController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoggingController {
+
+    Logger logger = LogManager.getLogger(LoggingController.class);
+
+    @RequestMapping("/logmessage")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
